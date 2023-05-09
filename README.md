6d7e03309098f30159df56bc561b9dfb63608af1
daviddieuHE <92298816+daviddieuHE@users.noreply.github.com>
2023-05-09 15:58:26 +0200

    update VM

--- README.md
+++
@@ -1,2 +0,0 @@
-# ADMIN2023
-Repository relatif au projet d'admin réseau 2023
---
+++ b2b/index.php
@@ -0,0 +1,32 @@
+<?php
+// Configuration de la base de données
+$servername = "10.0.0.3";  // Nom du conteneur de base de données
+$username = "user";  // Nom d'utilisateur MySQL
+$password = "psw";  // Mot de passe MySQL
+$dbname = "woodytoys";  // Nom de la base de données
+
+// Connexion à la base de données
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Requête SQL pour récupérer les données de la table products
+$sql = "SELECT nom, prix, qty FROM products";
+$result = $conn->query($sql);
+
+// Affichage des données dans une table HTML
+if ($result->num_rows > 0) {
+    echo "<table>";
+    echo "<tr><th>Nom</th><th>Prix</th><th>Quantité</th></tr>";
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["nom"]."</td><td>".$row["prix"]."</td><td>".$row["qty"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+// Fermeture de la connexion
+$conn->close();
+?>
